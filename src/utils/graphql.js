e23d83094514cc22dc84522cbb084b30e3bee87a
Soren Schmidt <sorenbs@gmail.com>
2016-03-22 19:15:28 +0100

    includes wasn't transformed by babel. Going old fashioned...

--- src/utils/graphql.js
+++ src/utils/graphql.js
@@ -1,4 +1,4 @@
 export function isScalar (typeIdentifier) {
   const scalarTypes = ['String', 'Int', 'Float', 'Boolean', 'GraphQLID', 'Enum']
-  return scalarTypes.includes(typeIdentifier)
+  return scalarTypes.filter((x) => x === typeIdentifier).length > 0
 }
